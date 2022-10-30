4af01a3e52bbc5dfd9e50b9435c5531a87c4b9b9
mairkolm <oec.valle.art@gmail.com>
2022-10-30 16:27:03 +0000

    fix Grafana dashboard

--- configs/fabric/leaf2_agent_install.sh
+++
@@ -1,7 +0,0 @@
-sudo yum localinstall --disablerepo=* /home/admin/srl-ndk-git_0.1.0_linux_amd64.rpm -y
-sleep 2
-sudo sed -i "s/failure-threshold: 10/failure-action: wait=1/g" /etc/opt/srlinux/appmgr/ndk-git.yml
-sr_cli  "/ tools system app-management application app_mgr reload"
-sleep 5
-sr_cli --candidate-mode --commit-at-end < /home/admin/agent_config_leaf2.txt
-
